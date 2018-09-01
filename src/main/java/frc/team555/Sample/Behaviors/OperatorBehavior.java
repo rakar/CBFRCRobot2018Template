3da860858a3a00a37bc96375af6f3d01e80e2854
rakar <rich.kopelow@gmail.com>
2018-08-31 22:30:18 -0400

    Initial Commit

---
+++ src/main/java/frc/team555/Sample/Behaviors/OperatorBehavior.java
@@ -0,0 +1,35 @@
+package frc.team555.Sample.Behaviors;
+
+import frc.team555.Sample.Data.ControlData;
+import frc.team555.Sample.Data.RequestData;
+import org.montclairrobotics.cyborg.Cyborg;
+import org.montclairrobotics.cyborg.behaviors.CBBehavior;
+import org.montclairrobotics.cyborg.data.CBStdDriveControlData;
+
+public class OperatorBehavior  extends CBBehavior {
+
+    private RequestData rd = (RequestData) Cyborg.requestData;
+    private ControlData cd = (ControlData) Cyborg.controlData;
+    private CBStdDriveControlData intake = cd.intake;
+
+    public OperatorBehavior(Cyborg robot) {
+        super(robot);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+
+        if(rd.shootCube) {
+            intake.active=true;
+            intake.direction.setXY(0,1); // bad value - shoot cube?
+            intake.rotation = 0;
+        } else {
+            if(!intake.active) {
+                intake.active = true;
+                intake.direction.setXY(0, -.20); // bad value - Draw cube in?
+                intake.rotation = 0;
+            }
+        }
+    }
+}
---
+++ src/main/java/frc/team555/robot/Robot.java
@@ -0,0 +1,7 @@
+package frc.team555.robot;
+
+import frc.team555.Sample.RobotCB;
+
+public class Robot extends RobotCB {
+
+}
